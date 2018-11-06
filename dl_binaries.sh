421546e6df884292b513b4b47ba3d1f71dfabdde
Jacob Jackson <jacobbfjackson@gmail.com>
2018-11-06 12:50:19 -0800

    Closes #2

---
+++ dl_binaries.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+# This script downloads the binaries for the most recent version of TabNine.
+
+version=$(curl -sS https://update.tabnine.com/version)
+targets=(
+    i686-apple-darwin
+    x86_64-apple-darwin
+    x86_64-unknown-linux-gnu
+    x86_64-pc-windows-gnu
+    i686-unknown-linux-gnu
+    i686-pc-windows-gnu
+)
+for target in ${targets[@]}
+do
+    mkdir -p binaries/$version/$target
+    case $target in
+        *windows*) exe=TabNine.exe ;;
+        *) exe=TabNine ;;
+    esac
+    path=$version/$target/$exe
+    echo "downloading $path"
+    curl -sS https://update.tabnine.com/$path > binaries/$path
+    chmod +x binaries/$path
+done
