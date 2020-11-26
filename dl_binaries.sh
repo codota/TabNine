4d244d560f163d9599a4c669841bd6704922b942
amircodota <amirtu@codota.com>
2020-11-26 10:26:32 +0200

    modify dl_binaries script (#302)

--- dl_binaries.sh
+++ dl_binaries.sh
@@ -3,23 +3,22 @@ set -e
 
 # This script downloads the binaries for the most recent version of TabNine.
 
-version="$(curl -sS https://update.tabnine.com/version)"
-targets='i686-apple-darwin
+version="$(curl -sS https://update.tabnine.com/bundles/version)"
+targets='i686-pc-windows-gnu
+    i686-unknown-linux-musl
     x86_64-apple-darwin
-    x86_64-unknown-linux-gnu
     x86_64-pc-windows-gnu
-    i686-unknown-linux-gnu
-    i686-pc-windows-gnu'
+    x86_64-unknown-linux-musl'
+
+rm -rf ./binaries
 
 echo "$targets" | while read target
 do
     mkdir -p binaries/$version/$target
-    case $target in
-        *windows*) exe=TabNine.exe ;;
-        *) exe=TabNine ;;
-    esac
-    path=$version/$target/$exe
+    path=$version/$target
     echo "downloading $path"
-    curl -sS https://update.tabnine.com/$path > binaries/$path
-    chmod +x binaries/$path
+    curl -sS https://update.tabnine.com/bundles/$path/TabNine.zip > binaries/$path/TabNine.zip
+    unzip -o binaries/$path/TabNine.zip -d binaries/$path
+    rm binaries/$path/TabNine.zip
+    chmod +x binaries/$path/*
 done
